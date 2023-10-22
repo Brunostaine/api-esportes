31ef5f671f0baaa05477173d09582122795b296d
Bruno Staine <brunostaine@hotmail.com>
2023-10-22 11:13:42 -0300

    Tratamentos de erro da api

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
+
+  // throw new Error("Not implemented");
+
   return res.json({ ok: true });
 });
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
+import express, { Response, Request, NextFunction } from "express";
+import "express-async-errors";
 import { router } from "./routes";
-import express, { Router, Request, NextFunction } from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
-const port = 3333
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+    });
+  }
+  return res.status(500).json({
+    status: "Error",
+    message: "Erro interno no servidor.",
+  });
+});
+
+const port = 3333;
 
 app.listen(port, () => {
   console.log(`SERVER ONLINE on port ${port}`);
