797b4fbf5de4b76cb1ec1ac3b1c7a104939d92ee
Bruno Staine <brunostaine@hotmail.com>
2023-10-23 13:53:31 -0300

    Cadastrando usuario no banco

--- src/controllers/user/CreateUserController.ts
+++ src/controllers/user/CreateUserController.ts
@@ -1,28 +1,22 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../../services/user/CreateUserService";
 
-
-
 class CreateUserController {
-    async handle(request: Request, response: Response) {
-      const { name, email, password, roles } = request.body;
-  
-      console.log("Name:", name);
-      console.log("Email:", email);
-      console.log("Password:", password);
-  
-      const createUserService = new CreateUserService();
-  
-      const user = await createUserService.execute({
-        name,
-        email,
-        password,
-        roles
-      });
-  
-      return response.json(user);
-    }
-  }
+  async handle(request: Request, response: Response) {
+    const { name, email, password, roles } = request.body;
 
-  export {CreateUserController}
-  
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+      roles,
+    });
+    console.log(user)
+
+    return response.json(user);
+  }
+}
+
+export { CreateUserController };
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,17 +1,47 @@
+import prismaClient from "../../prisma";
+import { hash } from "bcryptjs";
+
 interface UserRequest {
-    name: string;
-    email: string;
-    password: string;
-    roles: string[]
+  name: string;
+  email: string;
+  password: string;
+  roles: String[];
 }
 
 class CreateUserService {
-    async execute({ name, email, password, roles }: UserRequest) {
-    console.log(name);
-    console.log(email);
-    console.log(password);
-    console.log(roles);
-    return { ok: true };
+  async execute({ name, email, password, roles }: UserRequest) {
+    if (!email) {
+      throw new Error("Email incorreto.");
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("Email já cadastrado.");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+        roles: JSON.stringify(roles),
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        roles: true,
+      }
+    });
+
+    return user;
   }
 }
 
