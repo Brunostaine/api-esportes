811c76ba48b7935de889c56c3dec748ae7933acc
Bruno Staine <brunostaine@hotmail.com>
2023-10-24 18:15:25 -0300

    Fazendo o login do usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.get("/", (req: Request, res: Response) => {
 });
 
 router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -38,7 +38,7 @@ class CreateUserService {
         name: true,
         email: true,
         roles: true,
-      }
+      },
     });
 
     return user;
